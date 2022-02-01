9a92345032ee75a17e0b42a552bd296bbd307cf6
shaunvan119 <shaunvan@live.com.au>
2022-02-01 14:34:43 +1100

    added confirm.js and map via mapbox to confirm page

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -15,8 +15,17 @@ const Map = () => {
       style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
       center: [-99.29011, 39.39172],
       zoom: 3,
-    });
-  });
+    })
+
+    addToMap(map)
+    })
+
+       const addToMap = (map) =>{
+         const marker1 = new mapboxgl.Marker()
+         .setLngLat([12.554729, 55.70651])
+         .addTo(map);
+       }
+  
 
   return <Wrapper id='map'></Wrapper>
 }
---
+++ pages/confirm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import tw from "tailwind-styled-components"
+import Map from './components/Map'
+
+const confirm = () => {
+  return (
+  
+  <Wrapper>
+     <Map/>
+     <RideContainer>
+     Ride Selector
+     Confirm Ride
+     </RideContainer>
+  </Wrapper>
+  )
+}
+
+export default confirm
+
+const RideContainer = tw.div`
+flex-1
+`
+
+const Wrapper = tw.div`
+flex h-screen flex-col
+`
